d6a63053bad6748a2e7ed127c62782bd6e36ca83
Smith Scarborough <smithscarborough@gmail.com>
2021-04-08 15:52:45 -0500

    adds user login,  bcrypt, session, and logout

--- app.js
+++ app.js
@@ -3,18 +3,37 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// add the session npm downloads so that we can encrypt user password
+const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const models = require('./models');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+const store = new SequelizeStore({ db: models.sequelize })
+// FYI: below is actually middleware, and is responsible for setting up the session and saving the session
+app.use(
+    session({
+        secret: 'pancakes',
+        resave: false,
+        saveUninitialized: false,
+        store: store,
+    })
+);
+store.sync();
+// FYI: middleware cont'd
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1/users', usersRouter);
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,97 @@
 var express = require('express');
 var router = express.Router();
+// import the models folder so that we can check if a user already exists:
+const models = require('../models');
+// install bcrypt (npm i bcrypt) and include up here so that we can use it to encrypt user password
+const bcrypt = require('bcrypt');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+// POST /api/v1/users/register
+router.post('/register', async (req, res) => {
+  // Check that the username and password exists on the request 
+  if (!req.body.username || !req.body.password) {
+    // including a return below, so that if this happens, it will stop running this function and prevent it from continuing down the page and running the rest of this code
+    return res.status(401).json({
+      error: 'Please include username and password'
+    });
+  }
+
+  // check database for existing user
+  const user = await models.User.findOne({
+    where: {
+      // check where the username is the same as the username that came in on the body of the request
+      username: req.body.username,
+    },
+  });
+  
+  // if exists, send error
+  if (user) {
+    return res.status(400).json({
+      error: 'Username already in use',
+    });
+  }
+
+  // hash password using bcrypt (npm i bcrypt, and require it at the top)
+  // note: the 10 below is the number of times it will do the calculation -- 10 is a good number b/c it's long enough where someone won't be able to use brute force to hack it, but not so long that it takes forever to render
+  const hash = await bcrypt.hash(req.body.password, 10)
+
+  // create user
+  const newUser = await models.User.create({
+    // pass the username and password to the DB
+    username: req.body.username,
+    password: hash,
+  });
+    // respond with success message
+    return res.status(201).json(newUser);
+    });
+
+  router.post('/login', async (req, res) => {
+    // check if everything we need is located on the req body
+        // if not there, send error
+    if (!req.body.username || !req.body.password) {
+      return res.status(400).json({
+        error: 'Please include username and password',
+      });
+    }
+    // find user from username
+    const user = await models.User.findOne({
+      where: {
+        username: req.body.username,
+      },
+    });
+      // if no user, send error
+    if (!user) {
+      return res.status(404).json({
+        error: 'No user with that username found',
+      });
+    }
+      // if they do exist, then we need to check the password
+      const match = await bcrypt.compare(req.body.password, user.password)
+        // if password doesn't match, send an error
+      if (!match) {
+        return res.status(401).json({
+          error: 'Password incorrect'
+        })
+      }
+      // when the user logs in, store the user in session
+      req.session.user = user;
+        // if password does match, store user in session and then respond with user info
+    
+      res.json(user);
+      });
+
+
+      router.get('/logout', (req, res) => {
+        // clear user data from session by setting their session equal to null
+        req.session.user = null;
+        // respond to the user once logout is complete (i.e. send success response)
+        res.json({
+          success: 'Logged out successfully',
+        })
+      })
 
 module.exports = router;
