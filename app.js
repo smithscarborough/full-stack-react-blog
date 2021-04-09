7eefe852e0ad2d9461fe2cb68f51d2420bd18564
Smith Scarborough <smithscarborough@gmail.com>
2021-04-09 14:13:01 -0500

    adds react

--- app.js
+++ app.js
@@ -29,11 +29,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 
 
 app.use('/', indexRouter);
 app.use('/api/v1/users', usersRouter);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'cli/build', 'index.html'))
+})
 
 module.exports = app;
